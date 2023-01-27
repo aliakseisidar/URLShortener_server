5e3ff09e6ed5b1902817d4b08112bf82ed09fe02
Aliaksei_Sidar <aliaksei_sidar@epam.com>
2023-01-27 14:10:56 +0300

    fix CORS errors

--- index.js
+++ index.js
@@ -6,10 +6,18 @@ const adminRouter = require("./admin/adminRouter");
 const redirectRouter = require("./redirect/redirectRouter");
 const { mongoDB_URI } = require("./config");
 
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/auth", authRouter);
 app.use("/url", urlRouter);
