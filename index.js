3180d7f5075fbf4d5233aa5cc83a3e8f07b22da7
Your Name <you@example.com>
2026-01-28 15:15:37 +0300

    fix CORS

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const corsOptions = {
   credentials: true,
   exposedHeaders: ['Location'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  //allowedHeaders: ['Content-Type', 'Authorization'],
   optionSuccessStatus: 200,
 };
 
